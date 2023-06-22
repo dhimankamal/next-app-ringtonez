6fc69733231e9a52468b729da0029170543c6869
kamal <dhimank079@gmail.com>
2023-06-22 07:35:34 +0530

    add api

---
+++ pages/api/types.ts
@@ -0,0 +1,57 @@
+export interface WordpressPostResponse {
+    id: number;
+    date: string;
+    date_gmt: string;
+    guid: {
+      rendered: string;
+    };
+    modified: string;
+    modified_gmt: string;
+    slug: string;
+    status: string;
+    type: string;
+    link: string;
+    title: {
+      rendered: string;
+    };
+    content: {
+      rendered: string;
+      protected: boolean;
+    };
+    excerpt: {
+      rendered: string;
+      protected: boolean;
+    };
+    author: number;
+    featured_media: number;
+    comment_status: string;
+    ping_status: string;
+    sticky: boolean;
+    template: string;
+    format: string;
+    meta: any[];
+    categories: number[];
+    tags: any[];
+    yoast_head: string;
+    fields: {
+      file_size: string;
+      file: string;
+      view: string;
+      download_count: string;
+      premium: boolean;
+      description: string;
+    };
+    _links: {
+      self: { href: string }[];
+      collection: { href: string }[];
+      about: { href: string }[];
+      author: { embeddable: boolean; href: string }[];
+      replies: { embeddable: boolean; href: string }[];
+      "version-history": { count: number; href: string }[];
+      "predecessor-version": { id: number; href: string }[];
+      "wp:attachment": { href: string }[];
+      "wp:term": { taxonomy: string; embeddable: boolean; href: string }[];
+      curies: { name: string; href: string; templated: boolean }[];
+    };
+  }
+  
---
+++ pages/api/update.ts
@@ -0,0 +1,80 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/lib/db";
+import { UploadApiOptions, v2 as cloudinary } from "cloudinary";
+import { WordpressPostResponse } from "./types";
+
+const baseUrl = process.env.BACKEND_URL;
+
+// configure Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+type Data = {
+  name: string;
+};
+
+const getPostData = async (page: number) => {
+  try {
+    const res = await fetch(`${baseUrl}posts?per_page=20&page=${page}`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const post: WordpressPostResponse[] = await res.json();
+    return post;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getURL = async (url?: string) => {
+  console.log("url+++++", url);
+  if (url) {
+    const options: UploadApiOptions = {
+      resource_type: "auto",
+      folder: "ringtonez",
+      use_filename: true,
+      unique_filename: false,
+      overwrite: true,
+    };
+    try {
+      // Upload the ringtone
+      const result = await cloudinary.uploader.upload(url, options);
+      return result.secure_url;
+    } catch (error) {
+      console.error(error);
+      return "";
+    }
+  }
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const posts: WordpressPostResponse[] = await getPostData(1);
+  for (const data of posts) {
+    const currentDate = new Date().toISOString();
+    const url = await getURL(data?.fields?.file);
+    console.log("uploadurl>>>>>>", url);
+    const update: any = {
+      postid: String(data.id),
+      slug: data.slug,
+      title: data.title.rendered,
+      categories: data.categories,
+      tags: data.tags,
+      url,
+    };
+    await prisma.post.upsert({
+      where: { slug: data.slug || "" },
+      update: update,
+      create: { ...update, date: currentDate, downloads: 0 },
+    });
+    console.log(update);
+  }
+  res.status(200).json({ name: "Kamal" });
+}
