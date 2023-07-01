947bac70888c9488d82df7be43211766ad010c9c
kamal <dhimank079@gmail.com>
2023-07-01 17:45:52 +0530

    sitemap add

---
+++ app/sitemap.ts
@@ -0,0 +1,31 @@
+import { MetadataRoute } from "next";
+import { prisma } from "@/lib/db";
+
+const web_base_url = process.env.NEXT_WEB_URL;
+
+async function getPosts() {
+  try {
+    return await prisma.post.findMany({
+      take: 50,
+      orderBy: {
+        date: "desc",
+      },
+    });
+  } catch (error) {
+    throw error;
+  }
+}
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await getPosts();
+  console.log("test>>>>>", posts);
+
+  const sitemaps = posts.map(item => {
+    return {
+      url: `${web_base_url}${item.slug}`,
+      lastModified: new Date(item.date).toISOString(),
+    };
+  });
+
+  return sitemaps;
+}
