d82074e7b2ab1b8ae54eb971a8689482bd1c90aa
kamal <dhimank079@gmail.com>
2023-07-08 18:30:15 +0530

    fix navbar

--- lib/components/navbar/index.tsx
+++ lib/components/navbar/index.tsx
@@ -39,7 +39,7 @@ export default function Navbar() {
     <>
       <nav className="backdrop-blur-xl bg-white/60  dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600 ">
         <div className="flex flex-wrap items-center justify-between mx-auto p-4 container">
-          <Link href="/" className="flex items-center">
+          <Link href="/" onClick={()=>setOpen(false)} className="flex items-center">
             <Logo className="w-8 fill-black" />
           </Link>
           <div className="flex">
@@ -97,7 +97,7 @@ export default function Navbar() {
           <ul className="text-center">
             {navlinks.map(({ name, href, id }) => (
               <li key={id}>
-                <Link
+                <Link onClick={()=>setOpen(false)}
                   href={href}
                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
